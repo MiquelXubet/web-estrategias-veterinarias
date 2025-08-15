74b9694b63b9c2336659cbc7b1106bbbeda0bb66
Miki <miquelxubet@gmail.com>
2025-08-15 14:59:37 +0200

    commit funcionalidad boton solicitar demo

--- src/components/Banner/Hero.jsx
+++ src/components/Banner/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Hero.css";
 import heroImage from "../../Images/banner.png"; // Usa tu imagen aquí
+import { Link } from "react-router-dom";
 
 function Hero() {
   return (
@@ -11,11 +12,13 @@ function Hero() {
       <div className="hero-content">
         <h1>Transforma tu clínica veterinaria</h1>
         <p>Marketing digital especializado para el sector veterinario.</p>
+        <Link to="/contacto">
+          <button className="hero-btn">Solicita una demo</button>
+        </Link>
         {/* <p>
           Aumenta tu visibilidad, atrae más clientes y mejora la comunicación
           con tus pacientes.
         </p> */}
-        <button className="hero-btn">Solicita una demo</button>
       </div>
     </section>
   );
